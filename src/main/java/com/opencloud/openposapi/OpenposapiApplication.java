2df0ae4057cf0fe79cc5f873baec09adff1ea30b
Ivan Chan <29january@gmail.com>
2017-01-07 19:36:12 +0800

    project initial.

---
+++ src/main/java/com/opencloud/openposapi/OpenposapiApplication.java
@@ -0,0 +1,12 @@
+package com.opencloud.openposapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenposapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenposapiApplication.class, args);
+	}
+}
---
+++ src/main/java/com/opencloud/openposapi/models/Member.java
@@ -0,0 +1,7 @@
+package com.opencloud.openposapi.models;
+
+/**
+ * Created by ivanchan on 1/7/17.
+ */
+public class Member {
+}
